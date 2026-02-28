d21bc81f86c098838806d4d524bd2e8138f0afaf
Anindya <anindyadc@gmail.com>
2026-03-01 00:37:46 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/lifesync/',   // 👈 THIS LINE FIXES YOUR ISSUE
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     allowedHosts: ['lifesync.dizikloud.top']
   }
 })
-
